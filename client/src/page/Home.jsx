78b80898d957b8fcd2b41453a7a044f3f03291ff
Sanika Patil <2611sanika@gmail.com>
2023-10-03 13:58:46 +0530

    Intial Commits

---
+++ client/src/page/Home.jsx
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from 'react';
+
+import { Card, FormField, Loader } from '../components';
+
+const RenderCards = ({ data, title }) => {
+  if (data?.length > 0) {
+    return (
+      data.map((post) => <Card key={post._id} {...post} />)
+    );
+  }
+
+  return (
+    <h2 className="mt-5 font-bold text-[#64ffa7] text-xl uppercase">{title}</h2>
+  );
+};
+
+const Home = () => {
+  const [loading, setLoading] = useState(false);
+  const [allPosts, setAllPosts] = useState(null);
+
+  const [searchText, setSearchText] = useState('');
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState(null);
+
+  const fetchPosts = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch('https://localhost:5173/api/v1/post', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (response.ok) {
+        const result = await response.json();
+        setAllPosts(result.data.reverse());
+      }
+    } catch (err) {
+      alert(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  
+ 
+
+  return (
+    <section className="max-w-7xl mx-auto ">
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-[42px] text-center ">Create Your Post</h1>
+        <p className="text-[rgb(4,5,5)] mt-2 text-center text-large ">Create your own post with a prompt and an image</p>
+      </div>
+
+      
+
+      <div className="mt-10">
+        {loading ? (
+          <div className="flex justify-center items-center">
+            <Loader />
+          </div>
+        ) : (
+          <>
+            
+          </>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default Home;
